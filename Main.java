976bb1ce9a9e186d8ca1f0e068fb43e5e9ed8cb2
JFank612 <36108855+JFank612@users.noreply.github.com>
2020-01-25 15:42:13 -0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+    //using println
+        System.out.println("My name is Joel Fankhauser.");
+        System.out.println("I am currently working on my Web Design Degree.");
+        System.out.println("I am excited about taking this class and learning about app programming.");
+        System.out.println("I am a huge Green Bay Packers fan.");
+        System.out.println("My two favorite hobbies, when available, are kayaking and snowboarding");
+
+    }
+}
